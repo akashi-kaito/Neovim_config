63b6c308f5c5fa94b71ca3c65f7105aff4878493
akashi-kaito <akahikaito75@gmail.com>
2024-05-19 00:39:40 +0900

    　コメン修正

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,12 +1,12 @@
-vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { noremap = true })  
-vim.api.nvim_set_keymap('n', '<C-J>', ':bnext<CR>', { noremap = true })   
-vim.api.nvim_set_keymap('n', '<C-K>', ':bprev<CR>', { noremap = true })   
+vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-J>', ':bnext<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-K>', ':bprev<CR>', { noremap = true })
 
---ターミナルをインサートモードで開く
+-- 下半分にターミナルを開く
 vim.cmd([[
   command! -nargs=* Vter split | wincmd j | resize 20 | terminal
 ]])
--- ターミナル起動時はESCでコマンドモードへ移行する
+--ターミナルをインサートモードで開く
 vim.cmd([[
   autocmd TermOpen * startinsert
 ]])
