eddf22f0eb64023fb40bb328d0ed09d0c3f667bd
akashi-kaito <akahikaito75@gmail.com>
2024-05-19 00:29:12 +0900

    LSPの設定を追加 TODO:フォーマッターの設定

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,6 +2,14 @@ vim.api.nvim_set_keymap('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-J>', ':bnext<CR>', { noremap = true })   
 vim.api.nvim_set_keymap('n', '<C-K>', ':bprev<CR>', { noremap = true })   
 
+--ターミナルをインサートモードで開く
+vim.cmd([[
+  command! -nargs=* Vter split | wincmd j | resize 20 | terminal
+]])
+-- ターミナル起動時はESCでコマンドモードへ移行する
+vim.cmd([[
+  autocmd TermOpen * startinsert
+]])
 -- telescopeのキーマップ
 vim.api.nvim_set_keymap(
   'n',
@@ -31,11 +39,39 @@ vim.api.nvim_set_keymap(
   { noremap = true }
 )
 
---ターミナルをインサートモードで開く
-vim.cmd([[
-  command! -nargs=* Vter split | wincmd j | resize 20 | terminal
-]])
--- ターミナル起動時はESCでコマンドモードへ移行する
-vim.cmd([[
-  autocmd TermOpen * startinsert
-]])
+
+-- LSP起動時のキーマップ
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(ctx)
+    local set = vim.keymap.set
+    set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = true })
+    set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = true })
+    set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { buffer = true })
+    set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { buffer = true })
+    set("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = true })
+    set("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { buffer = true })
+    set("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = true })
+    set("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = true })
+    set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = true })
+    set("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", { buffer = true })
+    set("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", { buffer = true })
+    set("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", { buffer = true })
+    set("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", { buffer = true })
+    set("n", "<space>;f", "<cmd>lua vim.lsp.buf.formatting()<CR>", { buffer = true })
+  end,
+})
+
+
+-- 補完のキーマップ
+local cmp = require("cmp")
+cmp.mapping = cmp.mapping.preset.insert({
+  ["<C-p>"] = cmp.mapping.select_prev_item(),
+  ["<C-n>"] = cmp.mapping.select_next_item(),
+  ['<C-l>'] = cmp.mapping.complete(),
+  ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+  ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  ["<C-Space>"] = cmp.mapping.complete(),
+  ['<C-e>'] = cmp.mapping.close(),
+  ["<CR>"] = cmp.mapping.confirm { select = true },
+})
+
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,82 @@
+return{
+  {
+    "neovim/nvim-lspconfig",
+    config=function()
+      require("mason-lspconfig").setup()
+    end
+  },
+  {
+    "williamboman/mason.nvim" ,
+     config=function()
+       require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup_handlers {
+        function(server_name)
+          require("lspconfig")[server_name].setup {
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+          }
+        end,
+      }
+    end
+  },
+  {
+     'hrsh7th/nvim-cmp',
+     dependencies ={
+       'hrsh7th/cmp-nvim-lsp',
+       'hrsh7th/cmp-buffer',
+       'hrsh7th/cmp-path',
+       'hrsh7th/vim-vsnip',
+       'hrsh7th/cmp-vsnip',
+       'onsails/lspkind.nvim',
+    },
+    config = function()
+      local lspkind = require('lspkind')
+      local cmp = require('cmp')
+
+      -- 補完
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = 'vsnip' },
+        },
+        experimental = {
+          ghost_text = false,
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = '...',
+          })
+        }
+      })
+
+      -- バッファの補完
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- コマンドラインの補完
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = "path" },
+        },
+      })
+
+    end
+  },
+}
+
